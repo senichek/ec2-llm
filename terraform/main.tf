67925523a3601ead8b6ddc90f0881b5b215354cc
senichek <senichek@gmail.com>
2025-06-16 20:43:32 +0200

    feat(terraform): added locals block

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,23 @@
+locals {
+  project     = "llm-ec2-box"
+  environment = "dev"
+}
+
+
 module "vpc" {
   source              = "./modules/vpc"
   vpc_cidr_block      = "10.0.0.0/16"
   public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
   availability_zones  = ["us-east-1a", "us-east-1b"]
-  project             = "LLM-ec2-box"
-  environment         = "dev"
+  project             = local.project
+  environment         = local.environment
 }
 
 module "ec2_sg" {
   source      = "./modules/security_group"
-  project     = "llm"
-  environment = "dev"
+  project     = local.project
+  environment = local.environment
   vpc_id      = module.vpc.vpc_id
   ssh_cidr    = "xx.xxx.xxx.xx/32" # Replace with your real IP (YOUR_IP/32)
   alb_sg_id   = module.alb_sg.alb_sg_id
@@ -19,15 +25,15 @@ module "ec2_sg" {
 
 module "alb_sg" {
   source      = "./modules/security_group_alb"
-  project     = "llm"
-  environment = "dev"
+  project     = local.project
+  environment = local.environment
   vpc_id      = module.vpc.vpc_id
 }
 
 module "key_pair" {
   source      = "./modules/key_pair"
-  project     = "llm"
-  environment = "dev"
+  project     = local.project
+  environment = local.environment
 }
 
 module "ec2" {
@@ -36,16 +42,16 @@ module "ec2" {
   instance_type       = "t2.micro"              # or g4dn.xlarge for GPU inference
   private_subnet_ids  = module.vpc.private_subnet_ids
   security_group_id   = module.ec2_sg.security_group_id
-  project             = "llm"
-  environment         = "dev"
+  project             = local.project
+  environment         = local.environment
   instance_profile_name = null
   key_pair_name       = module.key_pair.key_name
 }
 
 module "alb" {
   source            = "./modules/alb"
-  project           = "llm"
-  environment       = "dev"
+  project           = local.project
+  environment       = local.environment
   vpc_id            = module.vpc.vpc_id
   public_subnet_ids = module.vpc.public_subnet_ids
   ec2_instance_id   = module.ec2.instance_id
